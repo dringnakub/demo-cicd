e3ea52212daa7d4398d1e91b508139e5e469e403
jOme <preenun.s@tcrbank.com>
2020-12-03 15:36:48 +0700

    [FIX] comment getVisaDetail

--- backEnd/src/main/java/com/happy/shoppingcart/api/controller/TransactionController.java
+++ backEnd/src/main/java/com/happy/shoppingcart/api/controller/TransactionController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api/v1/transaction")
 public class TransactionController {
 
-    @PutMapping
-    public ResponseEntity<TransactionResponse> getVisaDetail(@RequestHeader(name = "X-Correlation-Id", required = true) String correlationId,
-                                                      @RequestBody VisaDetail request )throws Exception {
-        TransactionResponse response = new TransactionResponse;
-        ResponseEntity<TransactionResponse> responseEntity = ResponseEntity.status(HttpStatus.OK).body(response);
-        return responseEntity;
-    }
+    //@PutMapping
+    //public ResponseEntity<TransactionResponse> getVisaDetail(@RequestHeader(name = "X-Correlation-Id", required = true) String correlationId,
+     //                                                 @RequestBody VisaDetail request )throws Exception {
+        //TransactionResponse response = new TransactionResponse;
+        //ResponseEntity<TransactionResponse> responseEntity = ResponseEntity.status(HttpStatus.OK).body(response);
+        //return responseEntity;
+    //}
 
 }
