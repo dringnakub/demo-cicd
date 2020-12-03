95781993a881d4e7ccf29deb21d00f1c9b336194
Isares Samruayruen <isares.s@tcrbank.com>
2020-12-03 15:49:56 +0700

    [UPDATED] fix imported

--- backEnd/src/main/java/com/happy/shoppingcart/common/repo/LoyaltyConfigRepo.java
+++ backEnd/src/main/java/com/happy/shoppingcart/common/repo/LoyaltyConfigRepo.java
@@ -1,6 +1,6 @@
 package com.happy.shoppingcart.common.repo;
 
-import com.happy.shoppingcart.common.entities.ShoppingCart;
+import com.happy.shoppingcart.common.entities.LoyaltyConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoyaltyConfigRepo extends JpaRepository<LoyaltyConfig,Integer> {
