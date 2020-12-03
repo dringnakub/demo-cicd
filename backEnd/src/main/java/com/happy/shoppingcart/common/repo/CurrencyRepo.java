0536ee26935a69a29990e5020ac5de4e7a5d92c2
Isares Samruayruen <isares.s@tcrbank.com>
2020-12-03 15:50:21 +0700

    [CREATED] currency entity and repo

---
+++ backEnd/src/main/java/com/happy/shoppingcart/common/repo/CurrencyRepo.java
@@ -0,0 +1,9 @@
+package com.happy.shoppingcart.common.repo;
+
+import com.happy.shoppingcart.common.entities.Currency;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyRepo extends JpaRepository<Currency,String> {
+    
+}
