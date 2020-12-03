abe5d3b7e7255e1fb43d352d56ad3f4b47294af6
Chartchai <Chartchai.b@tcrbank.com>
2020-12-03 12:02:48 +0700

    [CREATED] transaction table

---
+++ databaseScript/transaction.sql
@@ -0,0 +1,47 @@
+-- -------------------------------------------------------------
+-- TablePlus 3.11.0(352)
+--
+-- https://tableplus.com/
+--
+-- Database: db
+-- Generation Time: 2563-12-03 12:00:22.3310
+-- -------------------------------------------------------------
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+DROP TABLE IF EXISTS `transaction`;
+CREATE TABLE `transaction` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `addr1` varchar(255) DEFAULT NULL,
+  `addr2` varchar(255) DEFAULT NULL,
+  `addr3` varchar(255) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `mobile_number` varchar(10) NOT NULL,
+  `shipping_id` int NOT NULL,
+  `cart_id` int NOT NULL,
+  `total` int NOT NULL,
+  `point` int NOT NULL,
+  `shipping_fee` int NOT NULL,
+  `date_time` datetime DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
