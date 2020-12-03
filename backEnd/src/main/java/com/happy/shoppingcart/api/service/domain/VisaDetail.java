3dcd009044f41e3c54b832d6ef96b423f1b697c0
jOme <preenun.s@tcrbank.com>
2020-12-03 15:38:25 +0700

    [ADD] Visa Detail

--- backEnd/src/main/java/com/happy/shoppingcart/api/service/domain/VisaDetail.java
+++ backEnd/src/main/java/com/happy/shoppingcart/api/service/domain/VisaDetail.java
@@ -6,6 +6,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VisaDetail {
+    private int transaction_id;
+    private String card_number;
+    private String card_holder;
+    private int cvv;
+    private String expire_month;
+    private String expireyear;
 
 
 }
