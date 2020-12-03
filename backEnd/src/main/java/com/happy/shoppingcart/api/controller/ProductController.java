5659c1ca1f40e15c4015cacf82663564ed529e00
Isares Samruayruen <isares.s@tcrbank.com>
2020-12-03 16:26:01 +0700

    [UPDATED] product api

--- backEnd/src/main/java/com/happy/shoppingcart/api/controller/ProductController.java
+++ backEnd/src/main/java/com/happy/shoppingcart/api/controller/ProductController.java
@@ -1,7 +1,10 @@
 package com.happy.shoppingcart.api.controller;
 
+import com.happy.shoppingcart.api.controller.domain.ProductResponse;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/product")
 public class ProductController {
 
-    @GetMapping("/add_cart")
+    @GetMapping("add_cart")
     public ResponseEntity<String> addCart(@RequestParam(name = "product_id") int productId,
                                             @RequestParam(name = "cart_id") int cartId){
         
         return ResponseEntity.ok().body("");
     }
 
-    @PostMapping("/api/v1/product/calculate")
+    @PostMapping("calculate")
     public ProductResponse calculateAll() {
         ProductResponse response = new ProductResponse();
         response.setStatusCode(200);
--- backEnd/src/main/java/com/happy/shoppingcart/common/repo/CurrencyRepo.java
+++ backEnd/src/main/java/com/happy/shoppingcart/common/repo/CurrencyRepo.java
@@ -5,5 +5,7 @@ import com.happy.shoppingcart.common.entities.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurrencyRepo extends JpaRepository<Currency,String> {
+
+    public Currency findByCurrencyCode(String currencyCode);
     
 }
