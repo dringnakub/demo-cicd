97360b395121ec31273d5c4f0a0f1294b5fc9220
Apirat J <apirat.j@tcrbank.com>
2020-12-04 14:54:56 +0700

    [gioigus] calculate

--- backEnd/src/test/java/com/happy/shoppingcart/api/service/ProductServiceTest.java
+++ backEnd/src/test/java/com/happy/shoppingcart/api/service/ProductServiceTest.java
@@ -1,15 +1,22 @@
 package com.happy.shoppingcart.api.service;
 
+import com.happy.shoppingcart.api.controller.domain.ProductResponse;
+import com.happy.shoppingcart.common.entities.LoyaltyConfig;
 import com.happy.shoppingcart.common.entities.ProductTb;
+import com.happy.shoppingcart.common.entities.Shipping;
+import com.happy.shoppingcart.common.entities.ShoppingCart;
 import com.happy.shoppingcart.common.repo.ProductTbRepo;
+import com.happy.shoppingcart.common.repo.ShoppingCartRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -22,6 +29,8 @@ class ProductServiceTest {
     private ProductService unitUnderTest;
     @Mock
     private ProductTbRepo ProductTbRepos;
+    @Mock
+    private ShoppingCartRepo shoppingRepos;
 
     @Test
     void getProductListWhenInputAgeAndGenderIsNull_resultIsReturnedByFindAll() {
@@ -89,5 +98,37 @@ class ProductServiceTest {
         assertEquals(1, resultList.size());
         assertEquals(15, resultList.get(0).getProductId());
         assertEquals("OMG-Gossip Girl", resultList.get(0).getProductName());
+    }
+
+    @Test
+    void getCartListWhenInputId_resultIsReturnedByFindById() {
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Shipping shipping = new Shipping();
+        ProductTb productTb = new ProductTb();
+        LoyaltyConfig loyalty = new LoyaltyConfig();
+
+        productTb.setProductId(15);
+        productTb.setProductName("OMG-Gossip Girl");
+        productTb.setPrice(BigDecimal.valueOf(24.95));
+        shoppingCart.setCartId(1);
+        shoppingCart.setProductId(15);
+        shipping.setShippingId(1);
+        shipping.setShippingName("Kerry");
+        shipping.setShippingRate(BigDecimal.valueOf(40));
+        loyalty.setRowId(1);
+        loyalty.setPointRate(100);
+
+//        given(shoppingRepos.findById(1)).willReturn(Optional.of(shoppingCart));
+
+        ProductResponse result = this.unitUnderTest.calculateAll(1, 1);
+
+        assertEquals(1, result.getCartId());
+//        assertEquals();
+//
+//
+//        assertEquals(1, result.size());
+//        assertEquals(15, resultList.get(0).getProductId());
+//        assertEquals("OMG-Gossip Girl", resultList.get(0).getProductName());
     }
 }
